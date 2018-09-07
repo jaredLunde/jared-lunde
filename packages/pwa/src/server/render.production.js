66cc2800ab19fb4e2f5b1986f7444d8379ccd98e
Jared Lunde <jared.lunde@gmail.com>
2018-09-07 09:19:10 -0600

    now caches the page render

--- packages/pwa/src/server/render.production.js
+++ packages/pwa/src/server/render.production.js
@@ -10,10 +10,43 @@ import robots from './templates/robots.production.txt'
 
 
 const render = template(minifyHTML(HTML))
+const PageCache = new Map()
+
+const middleware = [
+  function overwriteSend (req, res, next) {
+    var send = res.send
+
+    res.send = function (string) {
+      const body = string instanceof Buffer ? string.toString() : string
+
+      PageCache.set(req.url, {
+        body,
+        statusCode: res.statusCode,
+        headers: res.getHeaders()
+      })
+
+      send.call(this, body)
+    }
+
+    next()
+  },
+  function handleRequest (req, res, next) {
+    let page = PageCache.get(req.url)
+
+    if (page !== void 0) {
+      res.set(page.headers)
+      res.status(page.statusCode)
+      res.send(page.body)
+    } else {
+      next()
+    }
+  }
+]
 
 export const main = serverless(
   createRenderer({
     render,
+    middleware,
     clientStats,
     routes,
     robots,
--- packages/shared/src/ui/Cover.js
+++ packages/shared/src/ui/Cover.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Box} from 'styled-curls'
+import {TransitionFade} from '@jaredlunde/curls-addons'
 import SizeObserver from '@render-props/size-observer'
 import Picture from '@jaredlunde/react-picture'
 
@@ -8,20 +9,25 @@ export default function Cover (props) {
   return (
     <SizeObserver>
       {({sizeRef, width, height}) => (
-        <Box
-          flex
-          justify='center'
-          align='center'
-          w='100%'
-          h='100%'
-          pos='absolute'
-          z='0'
-          css='opacity: 0.24; max-height: 100%;'
-          innerRef={sizeRef}
-          data-strict
-        >
-          <Picture width={width} height={height} {...props} data-strict/>
-        </Box>
+        <TransitionFade slow from={0.01} to={0.24} delay={60} easing='swiftMove'>
+          {({className}) => (
+            <Box
+              flex
+              justify='center'
+              align='center'
+              w='100%'
+              h='100%'
+              pos='absolute'
+              z='0'
+              className={className}
+              css='max-height: 100%;'
+              innerRef={sizeRef}
+              data-strict
+            >
+              <Picture width={width} height={height} {...props} data-strict/>
+            </Box>
+          )}
+        </TransitionFade>
       )}
     </SizeObserver>
   )
